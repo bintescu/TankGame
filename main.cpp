ee6a76fec8e4523ee12cb19353d1fb58b01610e8
bintescu <bintescurobert@yahoo.com>
2025-05-18 20:03:21 +0300

    Add some prints

--- main.cpp
+++ main.cpp
@@ -16,10 +16,13 @@ int main()
  tanc::motor.init(29,99);
  char nume[30];
  char nume2[30];
+ cout<<"WASD si Q pentru primul tanc :)\n";
+ cout<<"IJKL si N pentru al doilea :)\n";
  cout<<"\n Introduceti numele tancului nr 1 : ";
  cin>>nume;
  cout<<"\n Introduceti numele tancului nr 2: ";
  cin>>nume2;
+ cout<<"ENTER BOSS\n";
  tanc *T=new tanc(nume,97,100,119,115,113,13,6);
  tanc *T2= new tanc(nume2,106,108,105,107,110,13,90);
  intrare.init();
